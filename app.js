77b745851178984f935f1a34685b818f5052f34c
Vineshgithub123 <101579229+Vineshgithub123@users.noreply.github.com>
2022-08-28 17:17:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { options } = require('nodemon/lib/config');
 const { application } = require('express');
 const app = new express()
 const db = 'mongodb+srv://Vinauthapp:OTJSLl0mTNZu2gi3@authapp.zkkbbzk.mongodb.net/?retryWrites=true&w=majority'
-require("dotenv").config('.env');
+
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
